9077a9be9ad770c08ad5f1f193877db36f4a38a1
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-05-11 22:50:02 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,20 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
         Calculator calculator = new Calculator();
         int add = calculator.add(3,5);
         if(add == (3+5)){
