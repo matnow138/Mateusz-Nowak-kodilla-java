fb26842d98d4309537d80c593a9467d2b297062e
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-05-11 22:12:39 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,2 +1,22 @@
-package com.kodilla.testing;public class TestingMain {
-}
+package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int add = calculator.add(3,5);
+        if(add == (3+5)){
+            System.out.println("it works");
+        } else {
+            System.out.println("Nope");
+        }
+        int substract = calculator.substract(8,3);
+        if (substract == (8-3)){
+            System.out.println("It works");
+        } else{
+            System.out.println("Nope");
+        }
+
+
+        }
+    }
