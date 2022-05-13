ec99603a8c107c7f6a7f374aee40bcc9706197be
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-05-13 21:29:42 +0200

    6.3.final

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,66 +1,63 @@
 package com.kodilla.testing.collection;
 
 import org.junit.jupiter.api.*;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @DisplayName("Test of odd numbers exterminator")
 public class CollectionTestSuite {
 
     @BeforeEach
-    public void before(){ System.out.println("Test case: start");}
+    public void before() {
+        System.out.println("Test case: start");
+    }
+
     @AfterEach
-    public void after(){System.out.println("Test case: end");}
+    public void after() {
+        System.out.println("Test case: end");
+    }
+
     @BeforeAll
-    public static void beforeAll(){System.out.println("Odd numbers test: begin");}
+    public static void beforeAll() {
+        System.out.println("Odd numbers test: begin");
+    }
+
     @AfterAll
-    public static void afterAll(){System.out.println("Odd numbers test: end");}
+    public static void afterAll() {
+        System.out.println("Odd numbers test: end");
+    }
 
     @DisplayName("Testing method with empty list")
     @Test
-    void testCaseEmptyList () {
+    void testCaseEmptyList() {
         //Given
-        List<Integer> emptyList = new ArrayList<>();
-        OddNumberExterminator numbers = new OddNumberExterminator();
+        List<Integer> emptyList = Collections.emptyList();
+        OddNumberExterminator exterminator = new OddNumberExterminator();
+
         //When
+        List<Integer> evenList = exterminator.exterminate(emptyList);
 
-        List<Integer> result = numbers.exterminate(emptyList);
         //Then
-        Assertions.assertTrue(numbers.isEmpty());
-
-        System.out.println("It Works!");
+        assertFalse(evenList.isEmpty());
     }
-    @DisplayName("Testing method with random int")
-    @Test
-    void testCaseFilledList () {
-        //Given
-        List<Integer> filledList = new ArrayList<>();
-        OddNumberExterminator numbers = new OddNumberExterminator();
-        Random random = new Random();
 
-        for(int i=0;i<100;i++) {
-            filledList.add(random.nextInt(100));
-        }
+    @DisplayName("Testing method with int")
+    @Test
+    void testCaseFilledList() {
+        //Given
+        List<Integer> filledList = List.of(1, 2, 3, 4, 5);
+        OddNumberExterminator exterminator = new OddNumberExterminator();
 
         //When
-
-        List<Integer> result = numbers.exterminate(filledList);
+        //List<Integer> result = exterminator.exterminate(filledList);
+        Integer[] aray = {1,2,3,4};
+        List<Integer>listOfInts = Arrays.asList(aray);
+        List<Integer> evenNumbers = exterminator.exterminate(listOfInts);
         //Then
-        for(Integer temp:result){
-
-            if(temp%2!=0){
-                System.out.println("Error");
-                break;
-            }
-
-            }
-        System.out.println("It works!");
-
+        assertEquals(Set.of( 2, 4), new HashSet<>(evenNumbers));
     }
 
 }
