2ef262fb918b36591880150931ae972c2ca32a59
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-05-13 08:53:08 +0200

    6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,30 +4,7 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-
-        Calculator calculator = new Calculator();
-        int add = calculator.add(3,5);
-        if(add == (3+5)){
-            System.out.println("it works");
-        } else {
-            System.out.println("Nope");
-        }
-        int substract = calculator.substract(8,3);
-        if (substract == (8-3)){
-            System.out.println("It works");
-        } else{
-            System.out.println("Nope");
-        }
 
 
         }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,4 +1,58 @@
 package com.kodilla.testing.forum;
 
+import com.kodilla.testing.user.SimpleUser;
+import org.junit.jupiter.api.*;
+
+@DisplayName("Forum Test Suite")
 public class ForumTestSuite {
+
+
+    @BeforeEach
+    public void before(){
+        System.out.println("Test case: begin");
+    }
+    @AfterEach
+    public void after(){
+        System.out.println("Test case: end");
+    }
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("Test suite: begin");
+    }
+    @AfterAll
+    public static void afterAll(){
+
+            System.out.println("Test Suite: end");
+
+    }
+    @DisplayName(
+            "When created SimpleUser with name, " + "then getUsername should return correct name"
+    )
+    @Test
+    void testCaseUsername() {
+        //Given
+        SimpleUser simpleUser = new SimpleUser("theForumUser", "John Smith");
+        //When
+        String result = simpleUser.getUsername();
+        String expectedResult = "theForumUser";
+
+        //Then
+        Assertions.assertEquals(expectedResult, result);
+
+    }
+@DisplayName(
+        "When created SimpleUser with realName, " + "then getUsername should return correct realName"
+)
+    @Test
+    void testRealName() {
+        //Given
+        SimpleUser simpleUser = new SimpleUser("theForumUser", "John Smith");
+        //When
+        String result = simpleUser.getRealName();
+        System.out.println("Testing " + result);
+
+        //Then
+        Assertions.assertEquals("John Smith", result);
+
+    }
 }
