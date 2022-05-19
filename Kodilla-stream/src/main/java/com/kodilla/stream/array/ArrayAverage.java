3675e51efb1c5c92ea37868e6cebac6727eb4dd0
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-05-19 21:34:24 +0200

    7.5

---
+++ Kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayAverage.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public final class ArrayAverage {
+    public void printList(int[] numbers){
+
+        IntStream.range(0,numbers.length)
+                .forEach(System.out::println);
+    }
+    public double getAverage(int[] numbers){
+        double average = IntStream.range(0, numbers.length)
+                .filter(Objects::nonNull)
+                .average().getAsDouble();
+
+        return average;
+    }
+}
---
+++ Kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayOperationsTestSuite {
+
+    ArrayAverage arrayAverage = new ArrayAverage();
+
+
+
+
+    //Test for printing - method printList - only for MNO
+   /* @Test
+    void testPrintOut(){
+        //Given
+        int[] testList = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+        //When
+        arrayAverage.printList(testList);
+        //Then
+
+    }*/
+
+    @Test
+    void testGetAverage(){
+        //Given
+        int[] testList = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+        //When
+        double average = arrayAverage.getAverage(testList);
+        //Then
+        assertEquals(9.5, average);
+    }
+
+}
