da42a5507d26d4bb87e697adcfe73a4155e8614f
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-06-29 15:27:50 +0200

    shop 0.1.1

--- GoodPatterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
+++ GoodPatterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -1,14 +1,31 @@
 package com.kodilla.good.patterns.challenges;
 
 public class ProductOrderService {
-
+    private int orderQuantity;
 
     public ProductOrderService(){
 
     }
+    public void buyProcess(Item item, int orderQuantity, User user){
+        if(orderQuantity<item.getQuantity()){
+            System.out.println("You can order it!");
+        }else{
+            System.out.println("Insufficient quantity!");
+        }
 
+    }
     public OrderDto order(Item item){
-        boolean isAvailable = item.
+        return null;
+    }
+
+    public static void main(String args[]){
+        User user = new User("Jan", "Kowalski", "Polska", "jan.kowalski@mail.com", "123456789");
+
+        Item item = new Item("Bike", 5);
+
+        ProductOrderService productOrderService = new ProductOrderService();
+        int quantity = 3;
+        productOrderService.buyProcess(item,quantity,user);
 
     }
 
