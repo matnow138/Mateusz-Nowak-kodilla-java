8cc74aec312e72dd9fa1bb4401a2f15771771a1a
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-07-07 21:41:44 +0200

    flights 0.6

--- GoodPatterns/src/main/java/com/kodilla/good/patterns/challenges/flights/AirportClass.java
+++ GoodPatterns/src/main/java/com/kodilla/good/patterns/challenges/flights/AirportClass.java
@@ -20,4 +20,22 @@ public class AirportClass{
     public String getName() {
         return name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AirportClass that = (AirportClass) o;
+
+        if (!name.equals(that.name)) return false;
+        return flights.equals(that.flights);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + flights.hashCode();
+        return result;
+    }
 }
