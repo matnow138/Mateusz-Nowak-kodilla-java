5b47992ed9b71b7005a45e0b07c4cbf0d4810aa8
unknown <mateuszp.nowak@gmail.com>
2022-09-30 11:27:43 +0200

    24.4

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/JavaCourse.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns2.observer.homework;
+
+public class JavaCourse extends HomeworkQueue{
+
+    public JavaCourse(){
+        super("Java Course");
+    }
+}
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/HomeworkQueueTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/HomeworkQueueTestSuite.java
@@ -9,13 +9,20 @@ public class HomeworkQueueTestSuite {
     @Test
     public void homeworkUpdate(){
         //Given
-        HomeworkQueue frontEndCourse = new FrontEndCourse();
+        HomeworkQueue jenny = new FrontEndCourse();
+        HomeworkQueue sam = new JavaCourse();
         MentorUser adam = new MentorUser("Adam");
-        frontEndCourse.registerMentor(adam);
+        MentorUser john = new MentorUser("John");
+
+        jenny.registerMentor(adam);
+        sam.registerMentor(john);
         //When
-        frontEndCourse.addHomework("I learned HTML");
-        frontEndCourse.addHomework("I know JS!");
+        jenny.addHomework("I learned HTML");
+        jenny.addHomework("I know JS!");
+        sam.addHomework("I know how to code!");
+        sam.addHomework("I know what Java is");
         //Then
         assertEquals(2,adam.getUpdateCount());
+        assertEquals(2,john.getUpdateCount());
     }
 }
